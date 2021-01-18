32ce08a9a52247b629cf4942c82c185ddd4daf8f
girorme <rodrigo.girorme@gmail.com>
2021-01-18 01:53:42 -0300

    Add route files/xlsx and json output

--- src/files/files.routes.config.ts
+++ src/files/files.routes.config.ts
@@ -1,5 +1,7 @@
 import { CommonRoutesConfig } from '../common/common.routes.config';
 import express from 'express';
+import XLSX from 'xlsx';
+import formidable from 'formidable';
 
 export class FilesRoutes extends CommonRoutesConfig {
     constructor(app: express.Application) {
@@ -7,12 +9,20 @@ export class FilesRoutes extends CommonRoutesConfig {
     }
 
     configureRoutes() {
-        this.app.route('/files')
+        this.app.route('/files/xlsx')
             .get((req: express.Request, res: express.Response) => {
                 res.status(200).send('List files')
             })
             .post((req: express.Request, res: express.Response) => {
-                res.status(200).send('Post to files')
+                const form = new formidable.IncomingForm();
+                form.parse(req, function(_err: any, _fields: any, files: any) {
+                    const f = files[Object.keys(files)[0]];
+                    const workbook = XLSX.readFile(f.path);
+                    const sheet_name_list = workbook.SheetNames[0];
+                    res.status(200).send(
+                        XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list])
+                    )
+                });
             });
 
         return this.app;
